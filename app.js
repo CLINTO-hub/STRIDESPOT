9753415da4ba8f054d07597ca03b5c1b515c4864
CLINTO-hub <clintogeorge007@gmail.com>
2023-09-10 18:04:12 +0530

    added atlas string

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/Stride_Spot')
+mongoose.connect('mongodb+srv://clintogeorge007:pv9ohQA6dnvkIczS@cluster0.zh3fhfm.mongodb.net/')
 
 const express = require('express');
 const app = express();
